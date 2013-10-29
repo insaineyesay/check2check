afda9a2183c68f161cf9f26707e5a8a6d4ebd723
insaineyesay <insaineyesay@gmail.com>
2013-10-29 16:13:07 -0400

    Added additional Bill Fields (creating bills)
    newAmount and newDate have been added. Also changes to the controller
    to update the view with this data and display it. Also clear the inputs
    after submission.

--- app/js/controllers/bills_controller.js
+++ app/js/controllers/bills_controller.js
@@ -4,60 +4,29 @@ App.BillsController = Ember.ArrayController.extend({
 
 		// Get the Bill title set by the new 'New Bill' text field
 			var title = this.get('newTitle');
-			if (!title.trim()) { return; }
+			var amount = this.get('newAmount');
+			var date = this.get('newDate');
+
+			if (!title.trim() && !amount.trim() && !date.trim()) { return; }
 			console.log('yay1');
 
 			// Create the New Bill Model
 			var bill = this.store.createRecord('bill', {
 				billName: title,
-				billAmount: '$35.00',
-				billDate: '11/13/13'
+				billAmount: '$' + amount,
+				billDate: date
 			});
 
 			console.log('yay2');
 			// Clear the "New Bill" text field
 			this.set('newTitle', '');
+			this.set('newAmount', '');
+			this.set('newDate', '');
+
 			console.log('yay3');
 			// Save it
 			bill.save();
 			console.log('yay4');
 		}
-
-		/*createAmount: function() {
-			// Get the Bill title set by the new 'New Bill' text field
-			var amount = this.get('newAmount');
-	
-			if (!amount.trim()) {return;}
-
-			// Create the New Bill Model
-			bill.set('amount', amount);
-			
-
-			// Clear the "New Bill" text field
-			this.set('newAmount', '');
-
-			// Save i
-			bill.save();
-		},
-
-		createDate: function() {
-			// Get the Bill title set by the new 'New Bill' text field
-			var date = this.get('newAmount');
-	
-
-			if (!date.trim()) {return;}
-
-			// Create the New Bill Model
-			var bill = this.store.createRecord('bill', {
-				billDate: date,
-			});
-
-			// Clear the "New Bill" text field
-
-			this.set('newDate', '');
-
-			// Save it
-			date.save();
-		}*/
 	}
 });
