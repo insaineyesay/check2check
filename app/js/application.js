ab022fd93cb0d86ed818013e3799feedb77237e0
insaineyesay <insaineyesay@gmail.com>
2013-11-04 17:10:19 -0500

    started over
    only using application.js and index.html until i get this figured out.
    By 'this' i mean the structure of the application

--- app/js/application.js
+++ app/js/application.js
@@ -1,3 +1,6 @@
-window.App = Ember.Application.create();
+window.App = Ember.Application.create({
+// Log routing transitions and changes
+  LOG_TRANSITIONS: true
+	});
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
