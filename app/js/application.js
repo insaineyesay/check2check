89959fce265617e0ab10c430973698effef33bcf
insaineyesay <insaineyesay@gmail.com>
2013-11-05 20:56:46 -0500

    Skeleton Built
    Added pages, and the routes for these pages. Added twitter bootstrap
    for the structure of the site. Having an issue with the each loop and
    the 'bill' object

--- app/js/application.js
+++ app/js/application.js
@@ -1,6 +1,55 @@
-window.App = Ember.Application.create({
+var App = Ember.Application.create({
 // Log routing transitions and changes
-  LOG_TRANSITIONS: true
+  LOG_TRANSITIONS: true,
+   // log when Ember generates a controller or a route from a generic class
+  LOG_ACTIVE_GENERATION: true,
+  // log when Ember looks up a template or a view
+  LOG_VIEW_LOOKUPS: true
 	});
 
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+// Routes
+App.Router.map(function() {
+
+	this.resource('index');
+	this.resource('getting started');
+	this.resource('finances');
+	this.resource('bills');
+	this.resource('reports');
+	this.resource('contact');
+
+});
+
+App.IndexRoute = Ember.Route.extend({
+	beforeModel: function() {
+		this.transitionTo('index');
+	}
+});
+
+App.BillsRoute = Ember.Route.extend({
+	model: function() {
+		return App.Bill.find();
+	}
+});
+
+// Controllers
+App.FinancesController = Ember.ArrayController.extend({
+
+});
+
+// Models
+App.Bill = Ember.Object.extend({
+	id: 1,
+	name: '',
+	amount: '',
+	date: '',
+	apr: ''
+});
+
+console.log(App.bills);
+// Adapters
+// App.Bill.adapter = Ember.Adapter.create();
+
+// JSON 
+App.Bill.url = "api/bill";
+App.Bill.collectionKey = "bill";
