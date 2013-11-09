fa06c861a106f341e547d1e068b380ac1f4f0033
insaineyesay <insaineyesay@gmail.com>
2013-11-09 10:01:04 -0500

    jQuery UI Datepicker now showing
    Got it to show, but its not passing the value through to the
    'createBillItem' method. Added a new library for converting jquery UI
    widgets to ember views

--- app/js/libs/jqui.js
+++ app/js/libs/jqui.js
@@ -1,175 +1,3 @@
-window.App = Ember.Application.create({
-// Log routing transitions and changes
-  LOG_TRANSITIONS: true,
-   // log when Ember generates a controller or a route from a generic class
-  LOG_ACTIVE_GENERATION: true,
-  // log when Ember looks up a template or a view
-  LOG_VIEW_LOOKUPS: true,
-  LOG_BINDINGS: true,
-  LOG_STACKTRACE_ON_DEPRECATION: true,
-  LOG_VERSION: true,
-  debugMode: true
-	});
-
-// Adapters
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
-
-// Routes
-App.Router.map(function() {
-
-	this.resource('index', {path: '/'});
-	this.resource('getting started');
-	this.resource('finances', function() {
-		this.resource('bills');
-	});
-	this.resource('reports');
-	this.resource('contact');
-
-});
-
-App.IndexRoute = Ember.Route.extend({
-	beforeModel: function() {
-		this.transitionTo('index');
-	}
-});
-
-App.BillsRoute = Ember.Route.extend({
-	model: function() {
-		return this.store.find('bill');
-	}
-});
-
-// Controllers
-App.FinancesController = Ember.Controller.extend({
-   actions: {
-    createBillItem: function() {
-
-    // Get the Bill title set by the new 'New Bill' text field
-    var title = this.get('newTitle');
-    var amount = this.get('newAmount');
-    var date = this.get('newDate');
-
-    if (!title.trim() && !amount.trim() && !date.trim()) { return; }
-    console.log('yay1');
-
-    // Create the New Bill Model
-    var bill = this.store.createRecord('bill', {
-            name: title,
-            amount: amount,
-            date: date
-    });
-
-    
-    console.log('yay2');
-    // Clear the "New Bill" text field
-    this.set('newTitle', '');
-    this.set('newAmount', '');
-    this.set('newDate', '');
-
-    console.log('yay3');
-    // Save it
-    bill.save();
-    console.log('yay4');
-      }
-    }
-});
-
-App.BillController = Ember.ObjectController.extend({
-	actions: {
-		removeBill: function() {
-		// Grab the corresponding bill model
-		var bill = this.get('model');
-      bill.deleteRecord();
-			bill.save();
-		},
-
-     editBill: function() {
-      //Grab the model
-      this.set('isEditing', true);
-    },
-
-    cancelEdit: function() {
-      this.set('isEditing', false);
-    },
-
-    doneEditing: function() {
-     var bill = this.get('model');
-      //set editing back to false
-      this.set('isEditing', false);
-      this.get('model').save();
-    },
-
-    acceptChanges: function() {
-      this.set('isEditing', false);
-
-      if (Ember.isEmpty(this.get('model.title'))) {
-        this.send('removeBill');
-      } else {
-        this.get('model').save();
-      }
-    }
-	}
-});
-
-App.BillsController = Ember.ArrayController.extend({
-  isEditing: false,
-
-  totalBills: function() {
-      return this.getEach('.billItem').length;
-      }.property('@each.billItem'),
-
-  inflection: function() {
-      var totalBills = this.get('totalBills');
-      return totalBills === 1 ? 'bill' : 'bills';
-        }.property('@each.billItem'),
-  
-  sumOfBills: function() {
-     var bills = this.getEach('amount');
-     console.log(bills);
-    var billTotals = bills.reduce(function(previousValue, currentValue, index, array) {
-    return parseInt(previousValue, 10) + parseInt(currentValue, 10);
-     });
-     return billTotals;
-    }.property('@each')
-
-});
-
-
-// Models
-App.Bill = DS.Model.extend({
-	name: DS.attr('string'),
-	amount: DS.attr('number'),
-	date: DS.attr('string')
-});
-
-App.Bill.FIXTURES = [
-{
-        id: 1,
-        name: 'Bill One',
-        amount: '45',
-        date: '11/1/13'
-},
-{
-        id: 2,
-        name: 'Bill Two',
-        amount: '45',
-        date: '11/1/13'
-},
-{
-        id: 3,
-        name: 'Bill Three',
-        amount: '45',
-        date: '11/1/13'
-}
-];
-
-// Views
-
-
-// JSON 
-
-
-// jQuery UI
 // Namespaces in Ember should be created with Ember.Namespace.create()
 JQ = Ember.Namespace.create();
 
@@ -312,28 +140,21 @@ JQ.Menu = Em.CollectionView.extend(JQ.Widget, {
     }
 });
 
+
 // Create a new Ember view for the jQuery UI Datepicker widget
-App.DatePicker = Ember.View.extend(JQ.Widget, {
+JQ.DatePicker = Em.View.extend(JQ.Widget, {
     uiType: 'datepicker',
-    uiOptions: ['disabled', 'altField', 'altFormat', 'appendText', 'autoSize',
-    'buttonImage', 'buttonImageOnly', 'buttonText', 'calculateWeek', 'changeMonth', 'changeYear',
-    'closeText', 'constrainInput', 'currentText', 'dateFormat', 'dayNames', 'dayNamesMin',
-    'dayNamesShort', 'defaultDate', 'duration', 'firstDay', 'gotoCurrent', 'hideIfNoPrevNext',
-    'isRTL', 'maxDate', 'minDate', 'monthNames', 'monthNamesShort', 'navigationAsDateFormat',
-    'nextText', 'numberOfMonths', 'prevText', 'selectOtherMonths', 'shortYearCutoff', 'showAnim',
-    'showButtonPanel', 'showCurrentAtPos', 'showMonthAfterYear', 'showOn', 'showOptions',
-    'showOtherMonths', 'showWeek', 'stepMonths', 'weekHeader', 'yearRange', 'yearSuffix'],
+    uiOptions: ['disabled', 'altField', 'altFormat', 'appendText', 'autoSize', 'buttonImage', 'buttonImageOnly', 'buttonText', 'calculateWeek', 'changeMonth', 'changeYear', 'closeText', 'constrainInput', 'currentText', 'dateFormat', 'dayNames', 'dayNamesMin', 'dayNamesShort', 'defaultDate', 'duration', 'firstDay', 'gotoCurrent', 'hideIfNoPrevNext', 'isRTL', 'maxDate', 'minDate', 'monthNames', 'monthNamesShort', 'navigationAsDateFormat', 'nextText', 'numberOfMonths', 'prevText', 'selectOtherMonths', 'shortYearCutoff', 'showAnim', 'showButtonPanel', 'showCurrentAtPos', 'showMonthAfterYear', 'showOn', 'showOptions', 'showOtherMonths', 'showWeek', 'stepMonths', 'weekHeader', 'yearRange', 'yearSuffix'],
     uiEvents: ['create', 'beforeShow', 'beforeShowDay', 'onChangeMonthYear', 'onClose', 'onSelect'],
 
     tagName: 'input',
     type: "text",
-    attributeBindings: ['type', 'value'],
-    valueBindings: "newDate"
+    attributeBindings: ['type', 'value']
 });
 
-App.Datepicker = App.DatePicker.extend({
+App.Datepicker = JQ.DatePicker.extend({
   dateFormat: 'yy-mm-dd', //ISO 8601
-  templateName: '_datepicker',
+
   onSelect: function(dateText, inst) {
     alert(dateText);
   },
@@ -345,7 +166,3 @@ App.Datepicker = App.DatePicker.extend({
       }
   }
 });
-
-
-
-
